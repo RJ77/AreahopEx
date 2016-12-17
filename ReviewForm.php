a6baf38924705994e127a133489accab9c842074
Ruturaj J <jadhavruturaj797@gmail.com>
2016-12-17 11:18:47 +0530

    Add files via upload

--- ReviewForm.php
+++ ReviewForm.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include("Connection.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +11,7 @@
 <body>
 <div class="container">
 <div class="row">
-<div class="col-md-6 col-md-offset-6"></div>
+<div class="col-md-6 col-md-offset-3">
 <form name="reviewForm" >
 
   
@@ -15,13 +19,24 @@
   <h4>Submit a Review</h4>
 
   <fieldset class="form-group">
-    <select class="form-control" id="select1">
-      <option value>Choose Hotel</option>
+    Choose Hotel :<select class="form-control" id="select1">
+      <?php 
+      $qry="SELECT * FROM hotel"; 
+      $res= mysql_query($qry); 
+      while($row = mysql_fetch_array($res)){
+      	echo "<option value='".$row['NAME']."'>".$row['NAME']."</option>";
+      }
+      ?>
     </select>
   </fieldset>
   <fieldset class="form-group">
-    <select class="form-control" id="select2">
-      <option value>Select Rating</option>
+    Select Rating:<select class="form-control" id="select2">
+      <option value="5">5</option>
+      <option value="4">4</option>
+      <option value="3">3</option>
+      <option value="2">2</option>
+      <option value="1">1</option>
+
     </select>
   </fieldset>
 
@@ -32,10 +47,54 @@
     <input  type="email" class="form-control" id="email" placeholder="xyz@example.com"  />
   </fieldset>
   <fieldset class="form-group">
-    <input type="submit" class="btn btn-primary pull-right" value="Submit Review" />
+  <a href="index.html" class="btn btn-primary pull-left">Home</a>
+    <input type="button" onclick="add()" class="btn btn-primary pull-right" value="Submit Review" />
   </fieldset>
 </form>
+ <table  class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>Date</th>
+                        <th>Hotel Name</th>
+                        <th>Review</th>
+                        <th>Stars</th>
+                        <th>User Email</th>
+                      </tr>
+                    </thead>
+                    <tbody id="items">
+                      
+                     </tbody>
+           </table>
 </div>
 </div>
+</div>
+ <script src="jQuery-2.1.4.min.js"></script>
+ <script type="text/javascript">
+ 	function add(){
+ 		var s1= $('#select1').val();
+ 		var s2= $('#select2').val();
+ 		var r= $('#review').val();
+ 		var e= $('#email').val();
+ 		 $.ajax({
+                                  url: 'addData.php',
+                                  type: 'POST',
+                                  data: {s1:s1,s2:s2,r:r,e:e},
+                                  dataType: 'json',
+                        				 success:function(result){
+                                        $("#items").empty();
+                                        $.each(result, function(i, val){
+                                        	 var tr = "<tr>" +
+                                                 "<td>"+ val.DATE + "</td>" +
+                                                 "<td>"+ val.HOTEL + "</td>" +
+                                                 "<td>"+ val.REVIEW + "</td>" +
+                                                 "<td>"+ val.STARS+ "</td>" +
+                                                 "<td>"+ val.EMAIL+ "</td></tr>";
+                                                 $(tr).appendTo("#items");
+                                        }); 
+                                        								}
+
+                });
+ 	}
+ </script>
 </body>
 </html>
